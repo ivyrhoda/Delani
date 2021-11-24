8bcf2197ad16d40fa2a196c918ae1a2e8d3c01ab
ivyrhoda <ivy.rhoda@student.moringaschool.com>
2021-11-24 21:03:46 +0300

    implement jquery

--- Compress/Delani-Studio-master/js/script.js
+++
@@ -1,7 +0,0 @@
- // WHAT WE DO SECTION
- $(document).ready(function() {
-  $('.click').click(function() {
-      $(this).children('.hide').slideToggle(1500);
-      $(this).children('.show').toggle(1200);
-    });
-  }); 
